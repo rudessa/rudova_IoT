4f1a8a66478fd821e4874ab7669126576ae4003c
Svetlana Rudova <149309473+rudessa@users.noreply.github.com>
2026-03-03 14:41:57 +0800

    commit
    Написали универсальную функцию

--- car/sketch/sketch.ino
+++ car/sketch/sketch.ino
@@ -4,24 +4,32 @@
 #define DIR_RIGHT 7
 #define SPEED_RIGHT 6
 
+#define FORWARD_LEFT LOW
+#define BACKWARD_LEFT HIGH
+#define FORWARD_RIGHT HIGH
+#define BACKWARD_LEFT LOW
+
+
+
+//Функция универсальная 
+
+void move(bool left_dir, int left_speed, bool right_dir, int right_speed) {
+  digitalWrite(DIR_LEFT, left_dir);
+  digitalWrite(DIR_RIGHT, right_dir);
+  analogWrite(SPEED_LEFT, left_speed);
+  analogWrite(SPEED_RIGHT, right_speed);
+
+}
+
+
 void setup(){
   pinMode(DIR_LEFT, OUTPUT);
   pinMode(DIR_RIGHT, OUTPUT);
   pinMode(SPEED_LEFT, OUTPUT);
   pinMode(SPEED_RIGHT, OUTPUT);
-
-  digitalWrite(DIR_LEFT, LOW);
-  digitalWrite(DIR_RIGHT, HIGH);
-  digitalWrite(SPEED_LEFT, LOW);
-  digitalWrite(SPEED_RIGHT, HIGH);
-
+  move(FORWARD_LEFT, 255, FORWARD_RIGHT, 0 );
   delay(2000);
-
-  digitalWrite(DIR_LEFT, low);
-  digitalWrite(DIR_RIGHT, LOW);
-  digitalWrite(SPEED_LEFT, HIGH);
-  digitalWrite(SPEED_RIGHT, LOW);
-
+  move(FORWARD_LEFT, 0, FORWARD_RIGHT, 0);
 }
 
 
